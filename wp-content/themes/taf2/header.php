57c643423a0c7354b46a11c0ef1ff8580d339c0f
Leah Bateman <lbateman@mcad.edu>
2015-07-05 20:59:05 -0400

    Week 5 Commit
    Bought and activated ACF Pro (including Repeater); added Contact and
    Directions pages; fixed menu system; worked on Auditions page

--- wp-content/themes/taf2/header.php
+++ wp-content/themes/taf2/header.php
@@ -12,6 +12,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
--- wp-content/themes/taf2/page-contact.php
+++ wp-content/themes/taf2/page-contact.php
@@ -1,9 +1,7 @@
 <?php
-/**
- * The Template for displaying all audition posts.
- *
- * @package taf
- */
+/*
+Template Name: Contact
+*/
 
 get_header(); ?>
 
@@ -12,49 +10,36 @@ get_header(); ?>
 
 			<!-- show information (left column) -->
 			<section class="main-content">
-				<section class="show-info">
-					<?php the_post_thumbnail(); ?>
-					<h1><?php the_title(); ?></h1>
-				</section> <!-- End of show-info -->
+				<h1>Contact Theatre@First</h1>
 
-				<section class="audition-info">
-					<h2>General Audition Information</h2>
-					<p><?php the_field('audition_dates'); ?></p>
-					<p><?php the_field('callback_date'); ?></p>
-					<p><?php the_field('audition_blurb'); ?></p>
-					<ul>
-						<li><?php the_field('audition_venue'); ?></li>
-						<li><?php the_field('audition_information_blurb'); ?></li>
-						<li><?php the_field('audition_form_blurb'); ?></li>
-						<li><?php the_field('audition_materials'); ?></li>
-						<li><?php the_field('audition_sides_blurb'); ?></li>
-						<li><?php the_field('calendar_blurb'); ?></li>
-					</ul>
-				</section> <!-- End of audition-info -->
+				<h2>Email</h2>
+					<table>
+						<?php
+							// check if the repeater field has rows of data
+							if( have_rows('email') ):
+						 		// loop through the rows of data
+		    					while ( have_rows('email') ) : the_row(); ?>
+		    						<tr>
+			    						<?php //get the email address for the mailto link
+			    						$addr = get_sub_field('email_address');
+								        // display a sub field value ?>
+								        <td><?php the_sub_field('email_purpose'); ?>:</td>
+							        	<td><a href="mailto:<?php $addr ?>"><?php the_sub_field('email_address'); ?></a></td>
+							        </tr>
+			    					<?php endwhile;
+								else :
+								    // no rows found
+									endif;
+						?>
+					</table>
 
-				<section class="performance-info">
-					<h3>About the Show</h3>
-					<p><?php the_field('performance_info'); ?></p>
-					<p><?php the_field('about_the_play'); ?></p>
-					<p><?php the_field('about_the_director'); ?></p>
-				</section><!-- End of performance-info -->
-				
-				<section class="character-descriptions">
-					<h3>Character Descriptions</h3>
-					<!-- <?php while (have_rows('character_descriptions')): the_row(); ?>
-						<section class="char-desc">
-							<?php the_sub_field('character_name'); ?>
-							<?php the_sub_field('character_description'); ?>
-						</section> <!-- End of char-desc -->
-					<!-- <?php endwhile; ?> -->
-				</section><!-- End of character-descriptions -->
-				
-				<section class="reservation-form">
-					<h2>Audition Reservation Form</h2>
-				</section><!-- End of reservation-form -->
-				
+				<h2>Phone</h2>
+				    <?php the_field('phone'); ?>
+				<h2>Postal Mail</h2>
+				    <?php the_field('postal_mail'); ?>
+
 			</section> <!-- main-content -->
-				
+
 			<!-- Sidebar (right column) -->
 			<section class="custom-sidebar">
 				<section class="sidebar-icons clear">
@@ -80,7 +65,7 @@ get_header(); ?>
 					<p>Theatre@First is an all-volunteer community theatre based in Somerville, MA. Founded in 2003, we fill a vital niche in the vibrant Davis Square arts scene. We draw upon the talents and support of individuals and organizations throughout the community to provide opportunities for our participants and audiences to experience quality live theatre in a variety of local venues. For more about our recent history, see our In the Press page.</p>
 				</section>
 			</section>
-				
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
